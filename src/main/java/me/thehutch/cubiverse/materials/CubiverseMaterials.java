16076ae520c84385f1e5ab22cd53f7b832860bf6
thehutch <th3hutch@yahoo.co.uk>
2013-05-30 18:21:08 +0100

    Added star material Signed-off-by:thehutch <th3hutch@yahoo.co.uk>

--- src/main/java/me/thehutch/cubiverse/materials/CubiverseMaterials.java
+++ src/main/java/me/thehutch/cubiverse/materials/CubiverseMaterials.java
@@ -2,12 +2,14 @@ package me.thehutch.cubiverse.materials;
 
 import org.spout.api.material.BlockMaterial;
 import org.spout.api.material.basic.BasicSolid;
+
 /**
  * @author thehutch
  */
 public class CubiverseMaterials {
 
-	//Solids
+	//Planet Rocks
 	public static final BlockMaterial MOLTEN_ROCK = new BasicSolid("Molten Rock", "model://Cubiverse/materials/blocks/moltenrock/moltenrock.spm");
-
+	//Star Blocks
+	public static final BlockMaterial STAR = new BasicSolid("Star", "model://Cubiverse/materials/blocks/sun/sun.spm");
 }
