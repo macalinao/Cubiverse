33dd0afafb5bd6e022e0692323dc6cee7a13101c
thehutch <th3hutch@yahoo.co.uk>
2013-06-01 02:13:04 +0100

    beginning of sky effects for planets in the sky Signed-off-by:thehutch <th3hutch@yahoo.co.uk>

---
+++ src/main/java/me/thehutch/cubiverse/effects/CubiverseEffects.java
@@ -0,0 +1,12 @@
+package me.thehutch.cubiverse.effects;
+
+import org.spout.api.render.effect.RenderEffect;
+
+/**
+ * @author thehutch
+ */
+public class CubiverseEffects {
+
+	//Render Effects
+	public static final RenderEffect SKY = new SkyRenderEffect();
+}
---
+++ src/main/java/me/thehutch/cubiverse/effects/SkyRenderEffect.java
@@ -0,0 +1,18 @@
+package me.thehutch.cubiverse.effects;
+
+import org.spout.api.render.effect.RenderEffect;
+import org.spout.api.render.effect.SnapshotRender;
+
+/**
+ * @author thehutch
+ */
+public class SkyRenderEffect implements RenderEffect {
+
+	@Override
+	public void preRender(SnapshotRender snapshotRender) {
+	}
+
+	@Override
+	public void postRender(SnapshotRender snapshotRender) {
+	}
+}
